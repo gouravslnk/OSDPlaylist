c89ce5b519694ef63e06a72fc07294560513c5fb
codeblech <my.malikyash@gmail.com>
2024-07-11 11:52:33 +0530

    Added Song: A View to Kill

--- main.tf
+++ main.tf
@@ -40,6 +40,10 @@ data "spotify_track" "Zenith" {
   url = "https://open.spotify.com/track/0qKX14YZHptDWiEN0CgxGz?si=174ddb3f25414e2c"
 }
 
+data "spotify_track" "A_View_To_Kill" {
+  url = "https://open.spotify.com/track/7fN3QQtmCMkiczQ41IuhwK?si=d0e32a11da1f4c72"
+}
+
 resource "spotify_playlist" "playlist" {
   name        = "The CodeJam Playlist"
   description = "Wishing you make the nicest, most Randomly Accessible Memories this CodeR̶A̶M̶Jam :)"
@@ -51,5 +55,6 @@ resource "spotify_playlist" "playlist" {
     data.spotify_search_track.Odyssee.tracks[*].id,
     data.spotify_search_track.Parcels.tracks[*].id,
     data.spotify_track.Zenith.id,
+    data.spotify_track.A_View_To_Kill.id
   ])
 }
